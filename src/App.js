9b80a62c2792921eafd214238f486ee829ee1457
guimonteirojbv <guilhermemonteiroro@gmail.com>
2023-03-16 09:29:16 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/User/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = () => {
+  return <div>USUÁRIO</div>;
+};
+
+export default User;
